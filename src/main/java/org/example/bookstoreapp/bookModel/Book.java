fe4b87b78217ecd65063c4197c2d5995f307a7ce
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2025-03-20 19:16:24 +0330

    build a model of Book

---
+++ src/main/java/org/example/bookstoreapp/bookModel/Book.java
@@ -0,0 +1,16 @@
+package org.example.bookstoreapp.bookModel;
+
+public class Book {
+    private String title;
+    private String author;
+    private  String cover;
+    private Category category;
+    private double discount;
+    private double price;
+    private String isbn;
+    private double rating;
+    private int paperback;
+    private int year;
+    private int edition;
+    private String description;
+}
---
+++ src/main/java/org/example/bookstoreapp/bookModel/Category.java
@@ -0,0 +1,5 @@
+package org.example.bookstoreapp.bookModel;
+
+public enum Category {
+    BIOGRAPHY,CRYPTOGRAPHY,COMPUTER_SCIENCE,PERSONAL_DEVELOPMENT,PROGRAMMING_LANGUAGE
+}
