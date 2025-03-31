6d1e91da566ffdf93e91d88ac79086edfce483d7
amirziyacode <amzdidi@gmail.com>
2025-03-31 10:46:44 +0330

    interact with client

--- src/main/java/org/example/bookstoreapp/controller/BookController.java
+++ src/main/java/org/example/bookstoreapp/controller/BookController.java
@@ -6,16 +6,14 @@ import org.example.bookstoreapp.service.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/book")
+@CrossOrigin("http://localhost:5173")
 public class BookController {
 
     private final BookService bookService;
