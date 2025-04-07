abc1c23fdd5e5ddfa4fbecb8466972e0f6c0282a
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2025-04-07 21:04:09 +0330

    imp JwtService add dependency for jwtToken

---
+++ src/main/java/org/example/bookstoreapp/config/JwtAuthenticationFilter.java
@@ -0,0 +1,29 @@
+package org.example.bookstoreapp.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.example.bookstoreapp.repository.TokenRepo;
+import org.example.bookstoreapp.service.JwtService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final TokenRepo tokenRepo;
+    private final UserDetailsService userDetailsService;
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(
+          @NonNull HttpServletRequest request,
+          @NonNull  HttpServletResponse response,
+          @NonNull  FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
