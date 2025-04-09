633e548f56fde5eff64709c8302c514a8c84ff2b
amirziyacode <amzdidi@gmail.com>
2025-04-09 11:38:28 +0330

    remove all this file

--- src/main/java/org/example/bookstoreapp/Models/book/jwtToken/TokenType.java
+++
@@ -1,5 +0,0 @@
-package org.example.bookstoreapp.Models.book.jwtToken;
-
-public enum TokenType {
-    BEARER,
-}
--- src/main/java/org/example/bookstoreapp/Models/book/user/Role.java
+++
@@ -1,5 +0,0 @@
-package org.example.bookstoreapp.Models.book.user;
-
-public enum Role {
-    USER, ADMIN
-}
